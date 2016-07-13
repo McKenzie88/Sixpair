c49ac334aa7885bd180c88288eaa253f614c07ca
Daniel Schlaug <daniel.schlaug@gmail.com>
2016-07-14 08:43:09 +0900

    Support for writing to PS4 controller

--- SixaxisController.m
+++ SixaxisController.m
@@ -22,6 +22,10 @@ static SixaxisController *sharedInstance;
 
 static IOHIDDeviceRef gIOHIDDeviceRef = NULL;
 
+static long sonyVendorID = 0x054c;
+static long dualShock3ID = 0x0268;
+static long dualShock4ID = 0x05c4;
+
 long IOHIDDevice_GetVendorID(IOHIDDeviceRef inIOHIDDeviceRef);
 long IOHIDDevice_GetProductID(IOHIDDeviceRef inIOHIDDeviceRef);
 
@@ -41,27 +45,38 @@ static IOReturn Set_DeviceFeatureReport(IOHIDDeviceRef inIOHIDDeviceRef, CFIndex
 
 
 static IOReturn PS3_SetMasterBluetoothAddress(IOHIDDeviceRef inIOHIDDeviceRef, BluetoothDeviceAddress inBluetoothDeviceAddress) {
+    
+    printf("%s(%p)\n", __PRETTY_FUNCTION__, inIOHIDDeviceRef);
+    
+    long productID = IOHIDDevice_GetProductID(inIOHIDDeviceRef);
+    
+    if (productID == dualShock3ID) {
+        uint8_t report[8];
+        report[0] = 0x01; report[1] = 0x00;
+        memcpy(&report[2], &inBluetoothDeviceAddress, sizeof(inBluetoothDeviceAddress));
+        return(Set_DeviceFeatureReport(inIOHIDDeviceRef, 0xF5, report, sizeof(report)));
+    } else if (productID == dualShock4ID) {
+        
+        UInt8 mac[6];
+        memcpy(&mac[0], &inBluetoothDeviceAddress.data, sizeof(inBluetoothDeviceAddress));
+        uint8_t report[23] = { 0x13, mac[5], mac[4], mac[3], mac[2], mac[1], mac[0], 0x56, 0xE8, 0x81, 0x38, 0x08, 0x06, 0x51, 0x41, 0xC0, 0x7F, 0x12, 0xAA, 0xD9, 0x66, 0x3C, 0xCE };
+        return(Set_DeviceFeatureReport(inIOHIDDeviceRef, 0x13, report, sizeof(report)));
+    } else {
+        return -1;
+    }
 	
-	printf("%s(%p)\n", __PRETTY_FUNCTION__, inIOHIDDeviceRef);
-	
-	uint8_t report[8];
-	report[0] = 0x01; report[1] = 0x00;
-	memcpy(&report[2], &inBluetoothDeviceAddress, sizeof(inBluetoothDeviceAddress));
-	return(Set_DeviceFeatureReport(inIOHIDDeviceRef, 0xF5, report, sizeof(report)));
 	
 }
 static void Handle_DeviceMatchingCallback(void* inContext, IOReturn inResult, void* inSender, IOHIDDeviceRef inIOHIDDeviceRef) {
     
-    // Device VendorID/ProductID:   0x054C/0x0268   (Sony Corporation)
-    // Product ID of Playstation 4 controller: 0x05c4
     long vendorID = IOHIDDevice_GetVendorID(inIOHIDDeviceRef);
     long productID = IOHIDDevice_GetProductID(inIOHIDDeviceRef);
     
-    bool isPS4Controller = 0x05C4 == productID;
-    bool isPS3Controller = 0x0268 == productID;
-    bool isSonyProduct = 0x054C == vendorID;
+    bool isPS4Controller = dualShock4ID == productID;
+    bool isPS3Controller = dualShock3ID == productID;
+    bool isSonyProduct = sonyVendorID == vendorID;
     
-    if (isSonyProduct && ( isPS3Controller || isPS4Controller )) {
+    if (!isSonyProduct || !( isPS3Controller || isPS4Controller )) {
         return;
     }
     NSLog(@"Found PS3 Controller!");
